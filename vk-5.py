1282ec1e3349ca4fad07dc429ea1760f6e411ba3
doomch4nsk1y <157492031+doomch4nsk1y@users.noreply.github.com>
2024-03-29 09:23:25 +0300

    Update and rename vk-5.py to Модуль1 Задание 5.py

--- vk-5.py
+++
@@ -1,9 +0,0 @@
-numb1 = int(input())
-numb2 = float(input())
-numb3 = int(input())
-numb3 = str(int(format(numb3, 'b')))
-print(f"{numb1:0=+10}")
-print(f"{numb2:#>10.2f}")
-#print(f"{3:,=19}")
-numb3result = f"{numb3:0>16}"
-print('_'.join(numb3result[i:i+4] for i in range(0, len(numb3result), 4))[::+1])
