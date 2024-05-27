52f17fdeeeb7d37f23f19767cc8b5edb2efb0332
doomch4nsk1y <157492031+doomch4nsk1y@users.noreply.github.com>
2024-05-28 00:40:39 +0300

    Add files via upload

---
+++ Модуль 4 Задание 2.py
@@ -0,0 +1,10 @@
+numerator, denominator = int(input()), int(input())
+
+def changed_div(numerator, denominator):
+    try:
+        result = numerator / denominator
+    except ZeroDivisionError:
+        return denominator
+    return denominator / result
+
+print(changed_div(numerator, denominator))
